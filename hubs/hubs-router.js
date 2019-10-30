db5f788afea093631bf024ae00552aabd2c5dd22
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-10-30 10:54:59 +0100

    wip

--- hubs/hubs-router.js
+++ hubs/hubs-router.js
@@ -64,20 +64,14 @@ router.post('/', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  Hubs.remove(req.params.id)
-    .then(count => {
-      if (count > 0) {
-        res.status(200).json({ message: 'The hub has been nuked' });
-      } else {
-        res.status(404).json({ message: 'The hub could not be found' });
-      }
+router.delete("/:id", checkHubsId, (req, res) => {
+  Hubs.remove(req.hub.id)
+    .then(() => {
+      res.status(200).json({ message: "The hub has been nuked" });
     })
-    .catch(error => {
-      // log error to server
-      console.log(error);
+    .catch((error) => {
       res.status(500).json({
-        message: 'Error removing the hub',
+        message: `Error removing the hub: ${error.message}`
       });
     });
 });
